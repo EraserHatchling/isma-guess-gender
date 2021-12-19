0733c5ff8966b8737b0b6a7e204358567fe54aab
EraserHatchling <ismadaniel23sept@gmail.com>
2021-12-19 03:59:39 +0000

    commit1

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    let userName = "";
+    let userGender = "";
+async function getGender() {
+  fetch('https://api.genderize.io/?name=' + userName)
+	.then((response) => response.json())
+	.then((data) => {
+  	userName = data.name; 
+  	userGender = data.gender; 
+	});
+}
+</script>
+
+<h1>Guess Gender App</h1>
+
+<form on:submit|preventDefault={getGender}>
+    <label for="name">Name:</label>
+    <input id="userName" bind:value={userName} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Gender:{userGender}</p>
